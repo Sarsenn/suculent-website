d917cb7eb93d4d280df49cb1265d838ec25c45e1
Sarsen <blazer.fei@gmail.com>
2025-08-05 17:33:48 +0900

    new-header

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,11 @@ import { animation } from "./modules/animation";
 import { burgerMenu } from "./modules/burgerMenu";
 import { mobileMenu } from "./modules/mobileMenu";
 import { tabsNav } from "./modules/tabsNav";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-// ScrollSmoother requires ScrollTrigger
-import { ScrollSmoother } from "gsap/ScrollSmoother";
-import { SplitText } from "gsap/SplitText";
+
 
 document.addEventListener("DOMContentLoaded", () => {
-  let isMobile = false;
-  let logo = document.querySelector('.logo');
 
-//smoother scroll
-//fade in section 
-animation(gsap, ScrollSmoother, ScrollTrigger, logo);
 burgerMenu();
-mobileMenu(isMobile, logo);
 tabsNav();
 
 });
